ab5bade7dee60252ba6fb84358a7f6b605ffc9d6
hamzasmatii6 <80428587+hamzasmatii6@users.noreply.github.com>
2023-12-13 21:26:49 +0100

    Add files via upload

---
+++ src/app/showstudent/showstudent.component.spec.ts
@@ -0,0 +1,25 @@
+
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShowstudentComponent } from './showstudent.component';
+
+describe('ShowstudentComponent', () => {
+ 
+  let component: ShowstudentComponent;
+  let fixture: ComponentFixture<ShowstudentComponent>;
+
+  beforeEach(() => {
+    
+    TestBed.configureTestingModule({
+      declarations: [ShowstudentComponent]
+    });
+    fixture = TestBed.createComponent(ShowstudentComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/showstudent/showstudent.component.ts
@@ -0,0 +1,48 @@
+
+
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+
+
+interface Student{
+  id:number,
+ 
+  name: string,
+  
+  email: string,
+  gender:string
+}
+
+@Component({
+  selector: 'app-showstudent',
+  templateUrl: './showstudent.component.html',
+  styleUrls: ['./showstudent.component.scss']
+})
+
+export class ShowstudentComponent {
+  idc=0;
+  constructor(
+    private route: ActivatedRoute,
+   
+  ) {}
+  ngOnInit(): void {
+     this.idc = Number(this.route.snapshot.params['id']);
+     console.log(this.idc)
+  }
+  students: Student[] = [
+    {
+      id:1,
+    name: "aa",
+    email: "aa@email.com",
+    gender:"M"
+    },
+    {
+      id:2,
+      name:"bb",
+      email: "bb@email.com",
+      gender:"M"
+    }
+   
+  ];
+}
